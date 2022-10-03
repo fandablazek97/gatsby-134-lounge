d297074229de99180ddab172af8e9c8b430d09ad
fandablazek <blazek127@gmail.com>
2022-10-03 13:32:10 +0200

    Updated phone number

--- src/settings/de/navLinks.js
+++ src/settings/de/navLinks.js
@@ -25,7 +25,7 @@ export const mainRoutes = [
 // Call to action v navigaci - generují se z automaticky a první je v desktopové verzi
 export const callToAction = [
   {
-    label: "+420 720 555 322",
-    link: "tel:+420720555322",
+    label: "+420 606 736 710",
+    link: "tel:+420606736710",
   },
 ];
